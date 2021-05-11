d49de23101490ce7fe9fa99da7cd3d4d3771e94f
Howard Tai <howard@howardism.dev>
2021-05-11 09:04:53 +0800

    feat: add domain Quote

---
+++ reactive-backend/src/main/java/com/howardism/reactivebackend/domain/Quote.java
@@ -0,0 +1,12 @@
+package com.howardism.reactivebackend.domain;
+
+import lombok.Data;
+
+@Data
+public final class Quote {
+
+    private String id;
+    private String book;
+    private String content;
+
+}
